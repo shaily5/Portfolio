84a5c78e910e6d9a24075183078fb6e2736784a2
shaily5 <shailyp05@gmail.com>
2024-01-04 16:11:35 -0500

    projects updated.

--- src/projects.js
+++ src/projects.js
@@ -2,23 +2,22 @@ export const projects = [
   {
     _id: "1",
     title: "Medical ChatBot",
-    typo: 'MERN',
+    typo: 'Full Stack',
     content:
-      "Itchy eyes can be triggered by exposure to pollen, animal fur, mould, dust mites, make-up or eye drops.",
+      "Created a live medical chatbot using HTML, CSS, Bootstrap, Tailwind, and the MERN stack (MongoDB, Express, React, Node.js). This intelligent chatbot assists users in resolving their medical queries, provides suggestions regarding suitable medications for various health conditions.      ",
     category: "MERN stack React",
     image: "https://i.ibb.co/n1pT7f2/Chat-Bot-App.png",
-    link: "https://google.com",
-    badge: "",
+    link: "https://github.com/shaily5/MedicalChatbot"
   },
   {
     _id: "2",
     title: "Kalakari",
-    typo: "HTML CSS JS",
+    typo: "Front End",
     content:
       "Itchy eyes can be triggered by exposure to pollen, animal fur, mould, dust mites, make-up or eye drops.",
-    category: "HTML CSS JS ",
+    category: "Front End ",
     image: "https://i.ibb.co/VHWc4pk/Kalakari.png",
-    link: "https://kalakari.vercel.app/",
+    link: "https://shaily5.github.io/Kalakari/",
   },
   {
     _id: "3",
@@ -26,41 +25,60 @@ export const projects = [
     typo: 'HTML CSS JS',
     content:
       "Itchy eyes can be triggered by exposure to pollen, animal fur, mould, dust mites, make-up or eye drops.",
-    category: "HTML CSS JS ",
+    category: "Front End",
     image: "https://i.ibb.co/VCRxT1J/Recipe-API.png",
     link: "https://github.com/chintan0513/recipe",
   },
   {
     _id: "4",
-    title: "Potter DB",
-    typo: 'contribution',
+    title: "Pottervrese",
+    typo: '',
     content:
       "Itchy eyes can be triggered by exposure to pollen, animal fur, mould, dust mites, make-up or eye drops.",
-    category: "HTML CSS JS ",
-    image: "https://i.ibb.co/Yd4Xfkd/Potter-DB-Home.png",
-    link: "https://potterdb.com/",
+    category: "React.js, Javascript, TailwindCSS  ",
+    image: "",
+    link: "https://github.com/shaily5/Potterverse",
   },
   {
     _id: "5",
-    title: "Travel App",
-    typo: 'contribution',
+    title: "Music Recommender",
+    typo: 'Full Stack',
     content:
       "Itchy eyes can be triggered by exposure to pollen, animal fur, mould, dust mites, make-up or eye drops.",
-    category: "HTML CSS JS ",
-    image: "https://i.ibb.co/DgHTYYm/TravelGo.png",
-    link: "https://travgo.netlify.app/",
+    category: "React.js, Javascript, TailwindCSS  ",
+    image: "",
+    link: "https://github.com/shaily5/MusicRecommender",
   },
   {
     _id: "6",
-    title: "ToDo List",
-    typo: "JS",
+    title: "Aspiring Minds",
+    typo: 'Ruby, C#, Java, JavaScript',
     content:
-      "Itchy eyes can be triggered by exposure to pollen, animal fur, mould, dust mites, make-up or eye drops.",
-    category: "HTML CSS JS ",
-    image: "https://i.ibb.co/ZJZmF7v/To-Do-List.png ",
-    link: "https://todo-list-iota-nine.vercel.app/",
+      "A new Flutter project. ",
+    category: "React.js, Javascript, TailwindCSS  ",
+    image: "",
+    link: "https://github.com/shaily5/AspiringMinds/",
+  },
+  {
+    _id: "7",
+    title: "Global Search Engine",
+    typo: '',
+    content:
+      "A search engine is a tool that helps users to find information from the web. For the user,the search engine returns the most accurate matches from the internet. The project covered concepts like web crawling, handling HTML files, website searching, frequency analysis, and pattern evaluation.",
+    category: " ",
+    image: "",
+    link: "https://github.com/shaily5/GlobalSearchEngine",
+  },
+  {
+    _id: "8",
+    title: "Blogging App",
+    typo: 'Node.js, Express.js, React.js, MongoDB',
+    content:
+      "Created a Blog App using Node.js and Express.js with MongoDB that allows users to create, update, and delete blogs, as well as users add, update, and delete comments on those blogs.",
+    category: "React.js, Javascript, TailwindCSS  ",
+    image: "",
+    link: "https://github.com/shaily5/BloggingPlatform",
   }
-
 ];
 
 export default projects;
@@ -71,25 +89,15 @@ export const buttons = [
     value: "all"
   },
   {
-    name: "HTML CSS JS",
-    value: "HTML CSS JS"
+    name: "Front End",
+    value: "Front End"
   },
   {
     name: "React.JS",
     value: "react.js"
-  }, {
-    name: "Contribution",
-    value: "contribution"
-  }, {
-    name: "REST-API",
-    value: "rest-api"
   },
   {
-    name: "Bootstrap",
-    value: "bootstrap"
-  },
-  {
-    name: "MERN",
-    value: "MERN"
+    name: "Full Stack",
+    value: "Full Stack"
   }
 ];
